b0386d46f3f73f21e08c033c71ea70b1ecf81db2
krlos98 <68961465+krlos98@users.noreply.github.com>
2020-09-18 12:03:36 -0500

    coreccion de if y final

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -6,22 +6,23 @@ for i in range(len(matriz_a)):
     for j in range(len(matriz_a[i])):
         if matriz_a[i][j] == 1:
             vector_r[0] += 1
-        if matriz_a[i][j] == 1:
+        if matriz_a[i][j] == 2:
             vector_r[1] += 1
-        if matriz_a[i][j] == 1:
+        if matriz_a[i][j] == 3:
             vector_r[1] += 1
-        if matriz_a[i][j] == 1:
+        if matriz_a[i][j] == 4:
             vector_r[2] += 1
-        if matriz_a[i][j] == 1:
+        if matriz_a[i][j] == 5:
             vector_r[4] += 1
-        if matriz_a[i][j] == 1:
+        if matriz_a[i][j] == 6:
             vector_r[5] += 1
-        if matriz_a[i][j] == 1:
+        if matriz_a[i][j] == 7:
             vector_r[6] += 1
-        if matriz_a[i][j] == 1:
+        if matriz_a[i][j] == 8:
             vector_r[7] += 1
-        if matriz_a[i][j] == 1:
+        if matriz_a[i][j] == 9:
             vector_r[8] += 1
-        if matriz_a[i][j] == 1:
+        if matriz_a[i][j] == 10:
             vector_r[9] += 1
-
+print(vector_r)
+print(np.sum(vector_r))
