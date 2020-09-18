b49d8f469b9bb5d534f56780a22a6e9d344af3f0
krlos98 <68961465+krlos98@users.noreply.github.com>
2020-09-18 11:58:04 -0500

    crear vector y usar fot

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,3 +1,6 @@
 import numpy as np
 matriz_a = np.random.randint(1, 11, (10, 10))
 print(matriz_a)
+r = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+for i in range(len(matriz_a)):
+    for j in range(len(matriz_a[i])):
